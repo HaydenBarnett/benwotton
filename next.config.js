081501405b01a8b1834b66ac54865fa65acdc6af
Hayden Barnett <hello@haydenbarnett.com>
2021-10-13 22:05:10 +1100

    Add next-secure-headers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const { createSecureHeaders } = require('next-secure-headers');
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['images.prismic.io', 'prismic-io.s3.amazonaws.com'],
+  },
+  async headers() {
+    return [{ source: "/(.*)", headers: createSecureHeaders() }];
   },
 }
